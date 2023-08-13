5d8b49724424490cae44d182a5c3a48ea3dbd5f1
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-08-14 01:41:30 +0900

    feat: Project start

---
+++ src/main/java/com/example/Aucsion_Product_Service/AucsionProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aucsion_Product_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucsionProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AucsionProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Aucsion_Product_Service/AucsionProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aucsion_Product_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucsionProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
