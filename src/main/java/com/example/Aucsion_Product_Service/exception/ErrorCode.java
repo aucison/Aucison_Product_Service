1764694775ce72752cbd38c26cce356f8f9351c6
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-09-11 23:53:29 +0900

    feat: add enum class(errorcode)

---
+++ src/main/java/com/example/Aucsion_Product_Service/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.example.Aucsion_Product_Service.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 상품입니다."),
+    SEARCH_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 검색어입니다."),
+    IMG_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이미지입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
